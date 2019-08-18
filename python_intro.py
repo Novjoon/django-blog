02e0acd088cc3746f6092362745fc33521d34aa5
Pjoon <Lion.novjoon@gmail.com>
2019-08-19 01:13:11 +0900

    Django app

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+print('Hello, Django!')
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola')
+    elif name =='joon':
+        print('joon')
+    else:
+        print('who are you')
+
+hi('joon')
